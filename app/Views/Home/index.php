e957601364607b607dbaaa3ade1f26b5c4e24e46
Kelvinphp <Kelvinphp@users.noreply.github.com>
2015-07-14 16:04:46 -0700

    Create index.php

---
+++ app/Views/Home/index.php
@@ -0,0 +1,8 @@
+<tbody>
+            <tr>
+<h1> Selamat Datang di SedherhanaFw !! </h1>
+<small> Silahkan Buat Aplikasi sesuai keinginan Sobat. </small>
+
+            </tr>
+        </tbody>
+    </table>
